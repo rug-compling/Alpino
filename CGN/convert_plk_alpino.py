5ba59b472f83a6f5a385dbc64bb43eaa081a6004
Gertjan van Noord <g.j.m.van.noord@rug.nl>
2019-11-05 11:21:42 +0100

    ignore <mu

--- CGN/convert_plk_alpino.py
+++ CGN/convert_plk_alpino.py
@@ -8,23 +8,27 @@ def output_current(id,sent):
 def main(argv):
     mid=argv[1]
     pattern = re.compile('<au id="(.*)" s=".*" tb=".*">')
+    ignore_pattern = re.compile('<mu ')
     sent = ""
     id = mid + "__" + "0"
-    
+    ignore = True
     for line in sys.stdin:
         match = pattern.match(line)
+        ignore_match = ignore_pattern.match(line)
         if match:
             output_current(id,sent)
             sent = ""
             id = mid + "__" + match.group(1)
-        else:
+            ignore = False
+        elif ignore_match:
+            ignore = True
+        elif not ignore:
             fields = line.split()
             word = fields[0]
             if sent:
                 sent = sent + " " + word
             else:
                 sent = word
-
     output_current(id,sent)
 
 
