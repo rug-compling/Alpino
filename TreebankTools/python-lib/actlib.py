a9bedc535c9061b48d5350c20d3535ced6584fba
Gertjan van Noord <g.j.m.van.noord@rug.nl>
2021-09-09 20:25:14 +0200

    new version is not yet fully working

--- TreebankTools/python-lib/actlib.py
+++ TreebankTools/python-lib/actlib.py
@@ -352,7 +352,7 @@ def xmlmatch_from_mem(xmldata, filename, query=None, stylesheet=None, params=Non
             result = stylesheet(doc, **params)
 
             # uitvoer naar stdout
-            sys.stdout.write(unicode(result))
+            result.write_output("-")
         else:
             print filename
 
