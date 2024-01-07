6edb31243a442a69602316c1f6a6dd5d1e5e7271
Gertjan van Noord <g.j.m.van.noord@rug.nl>
2024-01-07 19:48:53 +0100

    errormining

--- Lexicon/Build/misc.pl
+++ Lexicon/Build/misc.pl
@@ -5220,7 +5220,7 @@ m('te veel',        modal_adverb(comp), [te,meer]).  % temeer daar/omdat
 
 
 m(desnoods,         modal_adverb(prep),desnoods).
-m(centraal,         modal_adverb(prep),centraal). 
+m(centraal,         modal_adverb(prep),centraal). % centraal in het midden; op het veld
 m(dicht,            modal_adverb(prep),dicht).    % dicht tegen zich aan ; dicht bij de macht?
 m(diep,             modal_adverb(prep),diep).     % diep in blessuretijd
 m(dwars,            modal_adverb(prep),dwars).    % dwars door
