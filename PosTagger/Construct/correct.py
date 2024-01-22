b11bc44c6e11c0d1729b15f07f935d885fa3b1e8
Gertjan van Noord <g.j.m.van.noord@rug.nl>
2024-01-22 08:41:57 +0100

    errormining

--- PosTagger/Construct/correct.py
+++ PosTagger/Construct/correct.py
@@ -5,15 +5,21 @@ import sys
 def main():
     for line in sys.stdin:
         (w,t,k,i,j,h,p,r,c,s) = line.rstrip().split("|")
-        if t == "pronoun(nwh,thi,sg,both,both,indef,strpro)" and r == "één":
-            t = "pronoun(nwh,thi,sg,de,both,indef,strpro)"
-        if t == "adjective(ge_no_e(both))" and r == "bedroeven":
-            t = "adjective(no_e(both))"
-            r = "bedroefd"
-        if t == "adjective(ge_no_e(both))" and r == "bedroefd":
-            t = "adjective(no_e(both))"
-        if t == "modal_adverb(noun_prep)" and r == "ongeveer":
-            t = "modal_adverb(adv_noun_prep)"
+        if t == "adjective(ge_no_e(adv))"  and r == "gezien":
+            t = "adjective(ge_no_e(both))"
+        if t == "adjective(ge_no_e(adv),pp(bij))"  and r == "gezien":
+            t = "adjective(ge_no_e(both),pp(bij))"
+        if t == "adjective(ge_no_e(adv),fixed([[tegemoet]]))"  and r == "gezien":
+            t = "adjective(ge_no_e(both),fixed([[tegemoet]]))"
+        # if t == "pronoun(nwh,thi,sg,both,both,indef,strpro)" and r == "één":
+        #     t = "pronoun(nwh,thi,sg,de,both,indef,strpro)"
+        # if t == "adjective(ge_no_e(both))" and r == "bedroeven":
+        #     t = "adjective(no_e(both))"
+        #     r = "bedroefd"
+        # if t == "adjective(ge_no_e(both))" and r == "bedroefd":
+        #     t = "adjective(no_e(both))"
+        # if t == "modal_adverb(noun_prep)" and r == "ongeveer":
+        #     t = "modal_adverb(adv_noun_prep)"
         print("{}|{}|{}|{}|{}|{}|{}|{}|{}|{}".format(w,t,k,i,j,h,p,r,c,s))
 
 
