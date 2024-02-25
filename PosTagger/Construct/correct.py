eee23b6f4c7e66e5e3dd72c1da355b1abfae862d
Gertjan van Noord <g.j.m.van.noord@rug.nl>
2024-02-25 21:05:44 +0100

    new models

--- PosTagger/Construct/correct.py
+++ PosTagger/Construct/correct.py
@@ -11,15 +11,8 @@ def main():
             t = "adjective(ge_no_e(both),pp(bij))"
         if t == "adjective(ge_no_e(adv),fixed([[tegemoet]]))"  and r == "gezien":
             t = "adjective(ge_no_e(both),fixed([[tegemoet]]))"
-        # if t == "pronoun(nwh,thi,sg,both,both,indef,strpro)" and r == "één":
-        #     t = "pronoun(nwh,thi,sg,de,both,indef,strpro)"
-        # if t == "adjective(ge_no_e(both))" and r == "bedroeven":
-        #     t = "adjective(no_e(both))"
-        #     r = "bedroefd"
-        # if t == "adjective(ge_no_e(both))" and r == "bedroefd":
-        #     t = "adjective(no_e(both))"
-        # if t == "modal_adverb(noun_prep)" and r == "ongeveer":
-        #     t = "modal_adverb(adv_noun_prep)"
+        if t == "tag" and h == "normal(enumeration)":
+            t = "enumeration"
         print("{}|{}|{}|{}|{}|{}|{}|{}|{}|{}".format(w,t,k,i,j,h,p,r,c,s))
 
 
