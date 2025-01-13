c1ed2d577e2d46c7d8045b44feb779e9f6d11192
Gertjan van Noord <g.j.m.van.noord@rug.nl>
2025-01-13 15:38:50 +0100

    a few more abbrs.

--- Tokenization/libtok.c
+++ Tokenization/libtok.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58b6ecdf7059ac7bae185ddf66cba91784ea40d754885f3bd2638cd80d32e062
-size 76691271
+oid sha256:23540f33fe6dfb6b638658b8a2f128cd23cd8b71d6715b47270b27a126c16b32
+size 76785759
